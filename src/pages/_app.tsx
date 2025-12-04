30686d00e26ff623a03d2f2f8c00160768f56ab6
dev-AshishRanjan <94990764+dev-AshishRanjan@users.noreply.github.com>
2025-12-04 22:15:07 +0530

    chore: add html header metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import type { AppProps } from 'next/app';
 import { AuthProvider } from '../lib/auth';
 import '../styles/globals.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
+      <Head>
+        <title>SysSentinel | Console</title>
+        <meta name="description" content="Infrastructure Monitoring Without the Bloat" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/logo.png" />
+      </Head>
       <Component {...pageProps} />
     </AuthProvider>
   );
